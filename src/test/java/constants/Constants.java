ad50bb9781858a05679dd8966e223140e9e9387e
Marc Murray <mmarc.96@gmail.com>
2024-07-31 11:24:47 +0100

    Add Constants file with paths

---
+++ src/test/java/constants/Constants.java
@@ -0,0 +1,19 @@
+package constants;
+
+public class Constants {
+	public static String BASE_PATH = "https://petstore3.swagger.io/api/v3";
+	public static String PET_PATH = "/pet";
+	public static String SINGLE_PET_PATH = "/pet/{petId}";
+	public static String PET_FIND_BY_STATUS_PATH = "/pet/findByStatus";
+	public static String PET_FIND_BY_TAG_PATH = "/pet/findByTag";
+	public static String SINGLE_PET_UPLOAD_IMAGE_PATH = "/pet/{petId}/uploadImage";
+	public static String STORE_INVENTORY_PATH = "/store/inventory";
+	public static String STORE_ORDER_PATH = "/store/order";
+	public static String SINGLE_STORE_ORDER_PATH = "/store/order/{orderId}";
+	public static String USER_PATH = "/user";
+	public static String USER_CREATE_WITH_LIST_PATH = "/user/createWithList";
+	public static String USER_LOGIN_PATH = "/user/login";
+	public static String USER_LOGOUT_PATH = "/user/logout";
+	public static String SINGLE_USER_PATH = "/user/{username}";
+
+}
