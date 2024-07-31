56e30146a42b210e3bd40f5e2f48866508d57a0c
Marc Murray <mmarc.96@gmail.com>
2024-07-31 11:47:29 +0100

    Add POJO's

---
+++ src/test/java/pojos/Order.java
@@ -0,0 +1,72 @@
+package pojos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Order {
+
+	@JsonProperty("petId")
+	private int petId;
+
+	@JsonProperty("quantity")
+	private int quantity;
+
+	@JsonProperty("id")
+	private int id;
+
+	@JsonProperty("shipDate")
+	private String shipDate;
+
+	@JsonProperty("complete")
+	private boolean complete;
+
+	@JsonProperty("status")
+	private String status;
+
+	public void setPetId(int petId){
+		this.petId = petId;
+	}
+
+	public int getPetId(){
+		return petId;
+	}
+
+	public void setQuantity(int quantity){
+		this.quantity = quantity;
+	}
+
+	public int getQuantity(){
+		return quantity;
+	}
+
+	public void setId(int id){
+		this.id = id;
+	}
+
+	public int getId(){
+		return id;
+	}
+
+	public void setShipDate(String shipDate){
+		this.shipDate = shipDate;
+	}
+
+	public String getShipDate(){
+		return shipDate;
+	}
+
+	public void setComplete(boolean complete){
+		this.complete = complete;
+	}
+
+	public boolean isComplete(){
+		return complete;
+	}
+
+	public void setStatus(String status){
+		this.status = status;
+	}
+
+	public String getStatus(){
+		return status;
+	}
+}
